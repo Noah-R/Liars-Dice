53baaa0019ab93316168d6385439abf69157becc
Noah Rutkovsky <noah.rutkovsky@gmail.com>
2025-01-04 18:54:55 -0500

    node, express, webpack

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.use(express.static('dist'));
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+//
+
 const io = require('socket.io')(3000, {
     cors: {
         origin: ['http://localhost:8080'],
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    entry: './client.js',
+    output: {
+        filename: './index.js'}
+}
