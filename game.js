10bbd890f3d121bf86f5453ae07d1a10d76f4903
Noah Rutkovsky <noah.rutkovsky@gmail.com>
2025-01-05 18:59:06 -0500

    It sends players their dice

--- game.js
+++ game.js
@@ -1,115 +1,138 @@
-module.exports = {
-	shuffle: function (array) {
-		for (let i = array.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
 
-			const temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-		}
-		return array;
-	},
+const shuffle = function (array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
 
-	Player: class {
-		constructor(game, id) {
-			this.game = game;
-			this.id = id;
-			this.diceCount = 5;
-			this.dice = [];
-		}
+		const temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	return array;
+}
 
-		count(value) {
-			let amount = 0;
-			for (let i = 0; i < this.dice.length; i++) {
-				if (this.dice[i] == value) {
-					amount += 1;
-				}
+class Player{
+	constructor(game, socket) {
+		this.game = game;
+		this.socket = socket;
+		this.diceCount = 5;
+		this.dice = [];
+	}
+
+	count(value) {
+		let amount = 0;
+		for (let i = 0; i < this.dice.length; i++) {
+			if (this.dice[i] == value) {
+				amount += 1;
 			}
-			return amount;
 		}
+		return amount;
+	}
 
-		rollDice() {
-			this.dice = [];
-			for (let i = 0; i < this.diceCount; i++) {
-				this.dice.push(Math.floor(Math.random * 6) + 1);
+	rollDice() {
+		this.dice = [];
+		for (let i = 0; i < this.diceCount; i++) {
+			this.dice.push(Math.floor(Math.random() * 6) + 1);
+		}
+		//send them their dice
+	}
+
+	takeTurn(bid, turnPlayer) {
+		let challenge = false;
+		if (bid["bidder"] > -1) {
+			//ask to challenge
+		}
+		let amount = 0;
+		let pips = 0;
+
+		if (challenge == "Y") {
+			bid["challenger"] = turnPlayer;
+		}
+		else {
+			while (
+				amount < 1 ||
+				pips < 1 ||
+				pips > 6 ||
+				amount < bid["amount"] ||
+				pips < bid["pips"] ||
+				(amount == bid["amount"] && pips == bid["pips"])
+			) {
+				//ask amount
+				amount = 1;
+				//ask pips
+				pips = 1;
 			}
-			//send them their dice
+			bid["amount"] = amount;
+			bid["pips"] = pips;
+			bid["bidder"] = turnPlayer;
 		}
+		return bid;
+	}
+}
 
-		takeTurn(bid, turnPlayer) {
-			let challenge = false;
-			if (bid["bidder"] > -1) {
-				//ask to challenge
-			}
-			amount = 0;
-			pips = 0;
+class Game{
+	constructor() {
+		this.players = [];
+		this.playersLeft = 0;
+		this.bid = {};
+		this.turnPlayer = 0;
+	}
 
-			if (challenge == "Y") {
-				bid["challenger"] = turnPlayer;
-			} else {
-				while (
-					amount < 1 ||
-					pips < 1 ||
-					pips > 6 ||
-					amount < bid["amount"] ||
-					pips < bid["pips"] ||
-					(amount == bid["amount"] && pips == bid["pips"])
-				) {
-					//ask amount
-					amount = 1;
-					//ask pips
-					pips = 1;
-				}
-				bid["amount"] = amount;
-				bid["pips"] = pips;
-				bid["bidder"] = turnPlayer;
-			}
-			return bid;
-		}
-	},
-
-	Game: class {
-		constructor() {
-			this.players = [];
-			this.bid = {};
-			this.turnPlayer = 0;
-		}
-
-		addPlayer(id) {
-			this.players.push(new Player(this, id));
+	addPlayer(socket) {
+		if(this.playersLeft == 0){
+			this.players.push(new Player(this, socket));
 			shuffle(this.players);
 		}
+	}
 
-		round() {
-			for (let i = 0; i < this.players.length; i++) {
-				this.players[i].rollDice();
-			}
-			this.bid = { bidder: -1, amount: 0, pips: 0, challenger: -1 };
-			while (this.bid["challenger"] == -1) {
-				this.bid = this.players[this.turnPlayer].takeTurn(
-					this.bid,
-					this.turnPlayer
-				);
-				this.turnPlayer = (this.turnPlayer + 1) % this.players.length;
-				while (this.players[this.turnPlayer].diceCount == 0) {
-					this.turnPlayer =
-						(this.turnPlayer + 1) % this.players.length;
+	start(){
+		if(this.playersLeft == 0){
+			this.playersLeft = this.players.length;
+			while(this.playersLeft > 1){
+				this.round();
+				this.playersLeft = 0;
+				for(let i = 0; i < this.players.length; i++){
+					if(players[i].diceCount > 0){
+						this.playersLeft++;
+					}
 				}
 			}
-			let total = 0;
-			for (let i = 0; i < this.players.length; i++) {
-				total += this.players[i].count(this.bid["pips"]);
-			}
-			let loser = this.bid["challenger"];
-			if (total < this.bid["amount"]) {
-				loser = this.bid["bidder"];
-			}
-			this.players[loser].diceCount -= 1;
-			this.turnPlayer = loser;
+		}
+	}
+
+	round() {
+		for (let i = 0; i < this.players.length; i++) {
+			this.players[i].rollDice();
+			this.players[i].socket.emit('event', "Your dice "+this.players[i].dice);
+		}
+		this.bid = { bidder: -1, amount: 0, pips: 0, challenger: -1 };
+		while (this.bid["challenger"] == -1) {
+			this.bid = this.players[this.turnPlayer].takeTurn(
+				this.bid,
+				this.turnPlayer
+			);
+			this.turnPlayer = (this.turnPlayer + 1) % this.players.length;
 			while (this.players[this.turnPlayer].diceCount == 0) {
-				//todo: end the game if one player left
-				this.turnPlayer = (this.turnPlayer + 1) % this.players.length;
+				this.turnPlayer =
+					(this.turnPlayer + 1) % this.players.length;
 			}
 		}
-	},
-};
+		let total = 0;
+		for (let i = 0; i < this.players.length; i++) {
+			total += this.players[i].count(this.bid["pips"]);
+		}
+		let loser = this.bid["challenger"];
+		if (total < this.bid["amount"]) {
+			loser = this.bid["bidder"];
+		}
+		this.players[loser].diceCount -= 1;
+		this.turnPlayer = loser;
+		while (this.players[this.turnPlayer].diceCount == 0) {
+			//todo: end the game if one player left
+			this.turnPlayer = (this.turnPlayer + 1) % this.players.length;
+		}
+	}
+}
+
+module.exports = {
+	shuffle, Game, Player
+}
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+g1 = new game.Game()
+
 app.use(express.static('dist'));
   
 app.listen(port, () => {
@@ -17,14 +19,22 @@ const io = require('socket.io')(3000, {
 
 io.on('connection', socket => {
     console.log(socket.id);
+    g1.addPlayer(socket);
+    socket.emit('event', 'you are '+socket.id);
 
     socket.on('event', (data) => {
-       console.log(data);
-       socket.emit('event', "received "+data);
+        console.log(data);
+        socket.emit('event', "received " + data);
     });
 
 });
 
+setTimeout(() => {    
+    console.log("Starting the game");
+    g1.start();
+  }, "5000");
+
+//http://localhost:8080/index.html
 //socket.emit to everyone
 //socket.broadcast.emit to everyone else
 //socket.to(room).emit to room
