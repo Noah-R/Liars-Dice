68b5ca5e539aac20f9b6d4c2b1fa2e7dbb4ac8dc
Noah Rutkovsky <noah.rutkovsky@gmail.com>
2025-01-10 22:10:20 -0500

    React setup

--- client.js
+++ client.js
@@ -1,3 +1,16 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+
+function NavigationBar() {
+	// TODO: Actually implement a navigation bar
+	return <h1>Hello from React!</h1>;
+}
+
+const domNode = document.getElementById("output");
+const root = createRoot(domNode);
+root.render(<NavigationBar />);
+
+/*
 import { io } from "socket.io-client"
 
 const socket = io('http://localhost:3000');
@@ -11,4 +24,4 @@ socket.on('connect', () => {
     socket.on('message', (data) => {
         document.getElementById("output").innerHTML += data + "<br>";
      });
-});
+});*/
